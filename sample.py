7a920a3e1207077aa2a9f7957bacdd817680a0eb
HiMurakami <nbk.01409@gmail.com>
2018-02-22 23:48:40 +0900

    数字入力、進捗バー、出力ファイル名に日付追加

--- sample.py
+++ sample.py
@@ -1,6 +1,28 @@
 import pandas as pd
+from datetime import datetime
+from tqdm import tqdm
 
-c_range = range(2) 
+while True:
+    try:
+        print('Set USD Rate (default: 110)')
+        #print('USD: ', end='')
+        USD = float(input('USD: '))
+        break
+    except ValueError:
+        USD = 110.0
+        break
+while True:
+    try:
+        print('Set RMB Rate (default: 18)')
+        #print('USD: ', end='')
+        RMB = float(input('RMB: '))
+        break
+    except ValueError:
+        RMB = 18.0
+        break
+
+
+c_range = range(2)
 #読み込むCSVの列幅は決め打ちしてあとで整形が良い気がする
 #今回は整形せず決め打ちのまま
 
@@ -20,7 +42,7 @@ print("\ndf1: shrink")
 print(df1)
 ryou = len(df1.index)
 
-df2 = df1.sort_values(by=1) 
+df2 = df1.sort_values(by=1)
 print(df2)
 df2[2] = 0
 print("\ndf2: sort and add empty_column")
@@ -29,8 +51,10 @@ print(df2)
 d_list=[0]
 #print("\ndf3")
 #df3=df2
+pbar0 = tqdm(total=len(df2.index) )
 for i in range( len(df2.index) ):
     cnt=0
+    pbar0.update(1)
     for j in range( len(df2.index) ):
         if df2.iat[i,1] == df2.iat[j,1]:
 #            print(df2.iat[i,1] +"=="+ df2.iat[j,1])
@@ -41,10 +65,11 @@ for i in range( len(df2.index) ):
 #    print(cnt)
 #    print(d_list)
 #    print("")
-print(d_list)
+#print(d_list)
+pbar0.close()
 
 #df.reset_index(drop=True)
-df3 = df2.reset_index(drop=True).drop(d_list) 
+df3 = df2.reset_index(drop=True).drop(d_list)
 print("\ndf3: counting")
 print(df3)
 mount=0
@@ -55,4 +80,4 @@ print( mount)
 print("")
 print(len(df1.index)-1)#<REF>行の分を引く
 
-df3.to_csv("output.csv",header=False,index=False)
+df3.to_csv(datetime.now().strftime("output_%Y_%m%d_%H%M.csv"),header=False,index=False)
